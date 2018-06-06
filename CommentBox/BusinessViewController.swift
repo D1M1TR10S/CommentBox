c34331e9ed3a0cadd6dde985e51960b8efc6ba64
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-06-06 01:06:27 -0700

    We have a working MVP!!! GET requests retrieve comments and POST requests post comments to the database. Time for phase 2 :)

--- CommentBox/BusinessViewController.swift
+++ CommentBox/BusinessViewController.swift
@@ -5,37 +5,86 @@
 //  Created by Dimitrios Philliou on 6/1/18.
 //
 
+import Alamofire
 import GooglePlaces
 import UIKit
 
 
-class BusinessViewController: UIViewController {
+class BusinessViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     // Dictionary holding business info
     var businessDict : [String: Any] = [:]
     
+    //Dictionary holding all comments associated with a selected business
+    var commentDict : [Int:String] = [:]
+    
     //string variable to hold the user's comment
     var comment: String = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Filling labels with business info
+        businessName.text = businessDict["name"] as! String
+        businessAddress.text = businessDict["address"] as! String
+        businessSite.text = businessDict["website"] as! String
+        
+        print("commentDict: \(type(of: commentDict))\n\n\n\n\n\n\n\n\n")
+        //Filling list with dictionary values
+        for (_, val) in commentDict {
+            dataModel.append(val)
+        }
+        print("dataModel: \(dataModel)\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.estimatedRowHeight = 50
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+    
+    func postComment(_ comment: String) {
+        //API call. Append placeID to the end once routes are setup
+        let commentsURL = "http://167.99.107.99:5000/comments"
+        let parameters = ["placeID": businessDict["placeID"] as! String, "actual": comment]
+        print("parameters: \(parameters)")
+        Alamofire.request(commentsURL, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+            print("POST request response: \(response)\n\n\n\n\n\n\n\n\n\n\n")
+        }
+    }
     
     @IBOutlet weak var businessName: UILabel!
     @IBOutlet weak var businessAddress: UILabel!
     @IBOutlet weak var businessSite: UILabel!
 
     
+    @IBOutlet weak var tableView: UITableView!
+    //Declaring an array for the tableView
+    var dataModel : [String] = []
+
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataModel.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        //Fill the tableView with all of the comments stored in dataModel
+        cell.textLabel?.text = dataModel[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.textAlignment = .justified
+        return cell
+    }
+    
     @IBOutlet weak var postComment: UITextView!
     @IBAction func commentButton(_ sender: Any) {
         comment = postComment.text!
-        print("Comment: \(comment)")
+        postComment(comment)
+        print("\n\n\n\n\n\n - Comment: \(comment)\n\n\n\n\n\n")
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        businessName.text = businessDict["name"] as! String
-        businessAddress.text = businessDict["address"] as! String
-        businessSite.text = businessDict["website"] as! String
-        // Do any additional setup after loading the view.
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- CommentBox/ViewController.swift
+++ CommentBox/ViewController.swift
@@ -19,8 +19,8 @@ class ViewController: UIViewController {
     //Dictionary holding all information about a selected business
     var businessDict : [String:Any] = [:]
     
-    //Dictionary holding all comment associated with a selected business
-    var commentDict : [Int:Any] = [:]
+    //Dictionary holding all comments associated with a selected business
+    var commentDict : [Int:String] = [:]
     
     // Presents the Autocomplete view controller when the button is pressed.
     @IBAction func autocompleteClicked(_ sender: UIButton) {
@@ -33,6 +33,7 @@ class ViewController: UIViewController {
         if segue.identifier == "businessSegue" {
             let placeController = segue.destination as! BusinessViewController
             placeController.businessDict = businessDict
+            placeController.commentDict = commentDict
         }
     }
     
@@ -41,27 +42,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func downloadComments(_ businessID: String!) {
-        
+    func downloadComments(_ businessID: String!) -> Dictionary<Int, String> {
         //API call. Append placeID to the end once routes are setup
         let commentsURL = "http://167.99.107.99:5000/comments"
         Alamofire.request(commentsURL).responseJSON { response in
+            print("\n\n\n\n\n\n - response: \(response)\n\n\n\n\n\n")
             var json = JSON(response.data!)
-            print("Deserialized response: \(json)")
             for var i in 0...json.count {
                 if json[i]["placeID"].stringValue == businessID {
                     self.commentDict[json[i]["id"].intValue] = json[i]["actual"].stringValue
-                    print(json[i])
+                    print("json[i]: \(json[i])")
                 }
             }
-            print("commentDict: \(self.commentDict)")
         }
+        return(self.commentDict)
     }
     
 }
 
 extension ViewController: GMSAutocompleteViewControllerDelegate {
-    
 
     
     // Handle the user's selection.
@@ -72,14 +71,15 @@ extension ViewController: GMSAutocompleteViewControllerDelegate {
         print("Place dictionary: \(place)")
         let placeDict : [String: Any] = ["name": place.name,
                                          "address": place.formattedAddress,
-                                         "website": place.website!.absoluteString]
+                                         "website": place.website!.absoluteString,
+                                         "placeID": place.placeID]
 
         //let placeDict["website"] = place.website ?? ""
         //App crashes if place.website == nil. Need to debug.
         
         businessDict = placeDict
-        downloadComments("asdf1234")
-        //downloadComments(place.placeID)
+        commentDict = downloadComments(place.placeID)
+
         label.text = " " + place.name
         dismiss(animated: true, completion: nil)
     }
